748e258ed500d7d1e4ae11b66c0258a21a122f45
corriehibberd <corrie@sandersteadbells.org.uk>
2014-03-19 22:50:30 +0000

    friendly redirection

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -47,6 +47,23 @@ describe "signin page" do
 		describe "for non-signed-in users" do
 			let(:user) { FactoryGirl.create(:user) }
 
+			describe "when attempting to visit a protected page" do
+				before do
+					visit edit_user_path(user)
+					fill_in "Email",   with: user.email
+					fill_in "Password", with: user.password
+					click_button "Sign in"
+				end
+
+				describe "after signing in" do
+
+					it "should render the desired protected page" do
+						page.should have_selector('title', text: 'Edit user')
+					end
+				end
+			end
+		end
+
 			describe "in the Users controller" do
 
 				describe "visiting the edit page" do
@@ -59,7 +76,7 @@ describe "signin page" do
 					specify { response.should redirect_to(signin_path) }
 				end
 			end
-		end
+		
 
 		describe "as wrong user" do
 			let(:user) { FactoryGirl.create(user) }
@@ -76,6 +93,6 @@ describe "signin page" do
 				specify { response.should redirect_to(root_path) }
 			end
 		end
-
 	end
+	
 end
